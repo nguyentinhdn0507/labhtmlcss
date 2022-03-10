88435ae93d838831870814e511ae476ce28707f1
nguyentinhdn0507 <69786007+nguyentinhdn0507@users.noreply.github.com>
2022-03-10 09:15:49 +0700

    practice HTML CSS Bootstrap

---
+++ chap11/Lab1/scripts/script.js
@@ -0,0 +1,10 @@
+// Hamburger menu function
+
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
